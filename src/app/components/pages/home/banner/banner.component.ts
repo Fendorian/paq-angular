f439ef5e05897b5acc92fdd5c1c7af9a27981a42
fendorian <papic.stefan24@gmail.com>
2023-06-23 11:47:19 +0200

    fixing routing 2306

--- src/app/components/pages/home/banner/banner.component.ts
+++ src/app/components/pages/home/banner/banner.component.ts
@@ -64,7 +64,7 @@ export class BannerComponent implements OnInit {
         break;
       case 'hr':
         this.banner = [hrBanner];
-        this.buttonText = 'Produkti';
+        this.buttonText = 'Proizvodi';
         break;
       case 'de':
         this.banner = [deBanner];
